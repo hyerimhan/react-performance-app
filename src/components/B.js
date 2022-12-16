226e3aad5a2820097149b99ef765282b66e2f3ab
hyerimhan <hnhyerim@gmail.com>
2022-12-16 20:00:52 +0900

    Complete

--- src/components/B.js
+++ src/components/B.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-const Message = ({message}) => {
+const Message = React.memo(({message}) => {
   return <p>{message}</p>
-}
+})
 
-const ListItem = ({post}) => {
+const ListItem = React.memo(({post}) => {
+  console.log('ListItem component is Rendering')
   return (
     <li key={post.id}>
       <p>{post.title}</p>
     </li>
   )
-}
+})
 
-const List = ({posts}) => {
+const List = React.memo( ({posts, testFunction}) => {
+  console.log('List component is Rendering')
   return(
     <ul>
-      <li>
-        {posts.map(post => (
-          <ListItem key={post.id} post={post} />
-        ))}
-      </li>
+      {posts.map(post => (
+        <ListItem key={post.id} post={post} />
+      ))}
     </ul>
   )
-}
+})
 
 const B = ({message, posts}) => {
+  const testFunction = useCallback(() => {
+    
+  }, [])
+  console.log('B component is Rendering')
   return (
     <div>
       <h1>B Component</h1>
       <Message message={message} />
-      <List posts={posts}/>
+      <List posts={posts} testFunction={testFunction}/>
     </div>
   )
 }
